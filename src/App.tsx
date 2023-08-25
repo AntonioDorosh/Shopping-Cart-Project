b21599d7bd93b8707c9e433ea004d0d9830b6ab3
AntonioDorosh <antondoroshenko92@gmail.com>
2023-08-25 21:25:21 +0300

    making global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Favorite from "./pages/Favorite.tsx";
 import Home from "./pages/Home.tsx";
+
 
 const App = () => {
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import {router} from "./routing/router.tsx";
 import {ProductProvider} from "./context/ProductContext.tsx";
+import {createGlobalStyle} from "styled-components";
+
+export const Global = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #E7F6FF;
+    font-family: 'Inter', sans-serif;
+  }
+
+  li {
+    list-style-type: none;
+  }
+`
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+        <Global/>
         <ProductProvider>
             <RouterProvider router={router}/>
         </ProductProvider>
